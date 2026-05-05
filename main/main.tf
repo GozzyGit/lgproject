8fa7c482f8f2ac3c5de6fdb10ba9ea349cae69b2
GozzyGit <lee.gosling876@outlook.com>
2026-05-05 23:33:09 +0100

    added new serviceplan and logicapp modules

--- main/main.tf
+++ main/main.tf
@@ -18,3 +18,34 @@ module "key_vault" {
   location            = var.location
 }
 
+data "azurerm_subscription" "primary" {}
+
+module "logic_app" {
+  source = "./modules/logic_app"
+
+  name_prefix         = var.name_prefix
+  location            = var.location
+  resource_group_name = module.resource_group.name
+
+  app_service_plan_id        = azurerm_service_plan.logic.id
+
+  storage_account_name       = module.storage.name
+  storage_account_access_key = module.storage.primary_access_key
+
+  email_to = var.email_to
+}
+
+resource "azurerm_role_assignment" "cost_reader" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Cost Management Reader"
+  principal_id         = module.logic_app.principal_id
+}
+
+resource "azurerm_service_plan" "logic" {
+  name                = "${var.name_prefix}-logic-plan"
+  location            = var.location
+  resource_group_name = module.resource_group.name
+
+  os_type  = "Linux"
+  sku_name = "WS1"
+}
---
+++ main/modules/logic_app/variables.tf
@@ -0,0 +1,27 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "name_prefix" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "storage_account_access_key" {
+  type = string
+}
+
+variable "app_service_plan_id" {
+  type = string
+}
+
+variable "email_to" {
+  type = string
+}
