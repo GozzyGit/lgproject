88689cf8ea307bd53cac704b33f117c4953cfd13
Your Name <winlapo@example.com>
2026-05-17 23:44:07 +0100

    .

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,3 +1,44 @@
+output "resource_group_name" {
+  description = "Resource group name"
+  value       = azurerm_resource_group.main.name
+}
+
 output "storage_account_name" {
-  value = azurerm_storage_account.tfstate.name
+  description = "Storage account name"
+  value       = azurerm_storage_account.main.name
+}
+
+output "storage_account_id" {
+  description = "Storage account resource ID"
+  value       = azurerm_storage_account.main.id
+}
+
+output "storage_container_name" {
+  description = "Terraform state container name"
+  value       = azurerm_storage_container.tfstate.name
+}
+
+output "key_vault_name" {
+  description = "Key Vault name"
+  value       = azurerm_key_vault.main.name
+}
+
+output "key_vault_uri" {
+  description = "Key Vault URI"
+  value       = azurerm_key_vault.main.vault_uri
+}
+
+output "log_analytics_workspace_name" {
+  description = "Log Analytics workspace name"
+  value       = azurerm_log_analytics_workspace.main.name
+}
+
+output "log_analytics_workspace_id" {
+  description = "Log Analytics workspace ID"
+  value       = azurerm_log_analytics_workspace.main.id
+}
+
+output "location" {
+  description = "Azure region used for deployment"
+  value       = azurerm_resource_group.main.location
 }
