c915d5c84da355a580ad24191af49a212be477b7
GozzyGit <lee.gosling876@outlook.com>
2026-05-19 14:35:39 +0100

    removed old main section

--- main/modules/cost_function/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "storage_account_name" {}
-variable "storage_account_access_key" {}
-variable "name" {}
-variable "location" {}
-variable "resource_group_name" {}
--- main/modules/logic_app/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "principal_id" {
-  value = azurerm_logic_app_standard.finops.identity[0].principal_id
-}
