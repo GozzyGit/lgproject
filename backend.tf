07eb6ac1267010baecf626783899258817444a9d
GozzyGit <lee.gosling876@outlook.com>
2026-05-19 14:24:52 +0100

    terraform apply success

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "lgprojecttfstate"
+    storage_account_name = "lgprojecttfstate876"
     container_name       = "tfstate"
-    key                  = "lgproject.tfstate"
+    key                  = "terraform.tfstate"
   }
 }
--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,10 +1,14 @@
+# -------------------------
+# Terraform State Resources
+# -------------------------
+
 resource "azurerm_resource_group" "tfstate" {
   name     = "tfstate-rg"
   location = var.location
 }
 
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "lgprojecttfstate"
+  name                     = "lgprojecttfstate876"
   resource_group_name      = azurerm_resource_group.tfstate.name
   location                 = azurerm_resource_group.tfstate.location
   account_tier             = "Standard"
@@ -12,6 +16,34 @@ resource "azurerm_storage_account" "tfstate" {
 }
 
 resource "azurerm_storage_container" "tfstate" {
-  name                 = "tfstate"
-  storage_account_name = azurerm_storage_account.tfstate.name
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
+}
+
+# -------------------------
+# Main Project Resources
+# -------------------------
+
+resource "azurerm_resource_group" "main" {
+  name     = "lgresourcegp876"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "lgprojectstorage876"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "main" {
+  name                = "lgkeyvault876"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
 }
