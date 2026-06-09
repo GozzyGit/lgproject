0c3c5edc9ddd5bfcd1a9c54f2b626f14c3ef4fcf
GozzyGit <lee.gosling876@outlook.com>
2026-06-09 21:00:20 +0100

    removed crap

--- modules/root-infra/main.tf
+++ modules/root-infra/main.tf
@@ -17,20 +17,3 @@ module "keyvault" {
   rg_name  = module.rg.name
   location = var.location
 }
-
-<<<<<<< HEAD
-module "dashboard" {
-  source   = "../dashboard"
-  prefix   = var.prefix
-  rg_name  = module.rg.name
-  location = var.location
-}
-=======
-module "cost_report" {
-  source = "../cost-report-logicapp"
-
-  prefix   = var.prefix
-  location = var.location
-  rg_name  = module.rg.name
-}
->>>>>>> parent of ebc4839 (fully working dev and prod with azure tfstate (waiting for manual azure delections before applying))
