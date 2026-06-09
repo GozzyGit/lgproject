e04823b28380c65a157374ce2e4a3d04e1e3275d
GozzyGit <lee.gosling876@outlook.com>
2026-06-09 20:24:33 +0100

    Revert "fully working dev and prod with azure tfstate (waiting for manual azure delections before applying)"
    This reverts commit ebc483984fe156140bdc52a6b9aa10577bee6e88.

--- envs/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "lgprojecttfstate876"
-    container_name       = "tfstate"
-    key                  = "dev.tfstate"
-  }
-}
--- modules/root-infra/main.tf
+++ modules/root-infra/main.tf
@@ -18,9 +18,19 @@ module "keyvault" {
   location = var.location
 }
 
+<<<<<<< HEAD
 module "dashboard" {
   source   = "../dashboard"
   prefix   = var.prefix
   rg_name  = module.rg.name
   location = var.location
 }
+=======
+module "cost_report" {
+  source = "../cost-report-logicapp"
+
+  prefix   = var.prefix
+  location = var.location
+  rg_name  = module.rg.name
+}
+>>>>>>> parent of ebc4839 (fully working dev and prod with azure tfstate (waiting for manual azure delections before applying))
