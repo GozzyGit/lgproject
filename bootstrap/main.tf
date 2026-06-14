c64f972bdd8b2cb94699b02a2ac9f243d4ccb34d
GozzyGit <lee.gosling876@outlook.com>
2026-06-14 11:21:19 +0100

    .

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -4,24 +4,6 @@ data "azurerm_subscription" "current" {}
 # Terraform State Resources
 # -------------------------
 
-resource "azurerm_resource_group" "tfstate" {
-  name     = "tfstate-rg"
-  location = var.location
-}
-
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "lgprojecttfstate876"
-  resource_group_name      = azurerm_resource_group.tfstate.name
-  location                 = azurerm_resource_group.tfstate.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_id    = azurerm_storage_account.tfstate.id
-  container_access_type = "private"
-}
 
 
 resource "azuread_application" "tf" {
@@ -33,11 +15,11 @@ resource "azuread_service_principal" "tf" {
 }
 
 resource "azuread_application_federated_identity_credential" "github" {
-  application_object_id = azuread_application.tf.object_id
+  application_id = azuread_application.tf.id
 
   display_name = "github-oidc"
 
-  issuer   = "https://token.actions.githubusercontent.com"
+  issuer    = "https://token.actions.githubusercontent.com"
   audiences = ["api://AzureADTokenExchange"]
 
   subject = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main"
